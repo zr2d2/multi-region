8cbd2df5a902516203ee9a7865f55e00a169652e
Zach <zrowe007@gmail.com>
2018-12-29 22:04:33 +0000

    start of sinatra app

---
+++ hello.rb
@@ -0,0 +1,41 @@
+require 'sinatra'
+
+ips = ['localhost', '13.53.173.17']
+
+get '/hello/:name' do
+  name = params['name']
+
+  region = `curl http://localhost/region`
+
+  string = "Hello #{name}!"
+
+  stops = `curl http://localhost/regions/`
+  string += " I went to #{stops}."
+  
+  return string
+end
+
+get '/region' do
+  host = `curl http://169.254.169.254/latest/meta-data/hostname`
+
+  region = host.split('.')[1]
+
+  return region
+end
+
+get '/regions/:past?' do
+  stops = params['past'].split(',') if params['past']
+  stops ||= []
+
+  region = `curl http://localhost/region`
+
+  stops << region
+
+  if stops.size < 10
+    ip = ips.sample
+
+    hops = `curl http://#{ip}/regions/#{stops.join ','}`
+  end
+
+  hops ||= stops.join ','
+end
